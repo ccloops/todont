a73039f4f8f6950fa8d767b8a924bdb71037026f
Catherine Looper <loopercat@gmail.com>
2018-07-20 10:42:31 -0700

    added prop types to TextInput component

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 const TextInput = ({onChange, value}) => {
   return(
     <input type='text' value={value} onChange={onChange} />
   );
- 
+};
+
+TextInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default TextInput;
